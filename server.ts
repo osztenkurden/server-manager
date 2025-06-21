1c75a02123965e9dc5e1acf02f9b9453340f91fc
Hubert Walczak <hubert@blast.tv>
2025-06-21 13:11:05 +0200

    a

--- server.ts
+++ server.ts
@@ -57,5 +57,6 @@ export const stopServer = async () => {
 }
 
 export const writeToServer = (text: string) => {
+    console.log("trying to execute", text);
     server.process?.stdin.write(`${text}\n`);
 }
